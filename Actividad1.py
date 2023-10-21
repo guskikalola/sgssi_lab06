c667f2ba572821ecac25fed12fb92e32e326b69e
guskikalola <guskikalola@gmail.com>
2023-10-21 15:25:07 +0200

    Arreglado script para que cumpla las especificaciones
    + Listado de numero de ceros
    + Arreglado script para que no acepte el caso donde hay una \n de mas antes de la ultima fila

--- Actividad1.py
+++ Actividad1.py
@@ -27,6 +27,9 @@ def conseguir_primero_ord_crono(relacion_fich_nceros):
     # Devuelve el primer elemento por orden
     # cronologico de creacion del fichero
     # secuencia_con_mayor_ceros = max(secuencias_hex, key=secuencias_hex.get)
+    if len(relacion_fich_nceros) == 0:
+        raise "La lista no puede estar vacia"
+
     return max(relacion_fich_nceros, key=lambda x: relacion_fich_nceros[x]['fecha_modif'])
 
 def conseguir_ganador(relacion_fich_nceros):
@@ -36,9 +39,14 @@ def conseguir_ganador(relacion_fich_nceros):
     #    filtrada
     # 4. Devuelve el ganador
 
+    lista_filtrada = {k: v for k, v in relacion_fich_nceros.items() if v['valido']}
+
     mayor_num_ceros = conseguir_mayor_num_ceros(relacion_fich_nceros)
-    lista_filtrada = {k: v for k, v in relacion_fich_nceros.items() if v['longitud_0'] == mayor_num_ceros and v['validez']}
+    lista_filtrada = {k: v for k, v in relacion_fich_nceros.items() if v['longitud_0'] == mayor_num_ceros and v['valido']}
     
+    if len(lista_filtrada) == 0:
+        raise "No hay elementos validos en la carpeta"
+
     return conseguir_primero_ord_crono(lista_filtrada)
     
 
@@ -51,13 +59,21 @@ def conseguir_relacion_nceros(nombre_archivo_base, nombre_carpeta):
     for v in os.walk(nombre_carpeta, topdown=True):
         (_,_,files) = v 
         for file_name in files:
-            (validez,longitud_0,fecha_modif) = comprobar_validez(nombre_archivo_base,os.path.join(nombre_carpeta,file_name))
-            relacion_fich_nceros[file_name] = {"validez": validez, "longitud_0": longitud_0, "fecha_modif":fecha_modif}
+            (valido,longitud_0,fecha_modif) = comprobar_validez(nombre_archivo_base,os.path.join(nombre_carpeta,file_name))
+            relacion_fich_nceros[file_name] = {"valido": valido, "longitud_0": longitud_0, "fecha_modif":fecha_modif}
         return relacion_fich_nceros
+
+def imprimir_relacion_fich(relacion_fich_nceros):
+    lista_ordenada = {k: v for k, v in sorted(relacion_fich_nceros.items(), key=lambda x: (x[1]['valido'],x[1]['longitud_0'],x[1]['fecha_modif']),reverse=True)}
+    for (nombre_archivo,datos) in lista_ordenada.items():
+        str_longitud_0 = str(datos['longitud_0']) if datos['valido'] else 'No valido'
+        print(nombre_archivo + " : " + str_longitud_0)
 
 def main():
     relacion_fich_nceros = conseguir_relacion_nceros(p1_nombre_archivo_base,p2_nombre_carpeta_a_comprobar)
     ganador = conseguir_ganador(relacion_fich_nceros)
+
+    imprimir_relacion_fich(relacion_fich_nceros)
     
     print("Ganador: " + ganador)
 
--- modulos/Actividad5_3.py
+++ modulos/Actividad5_3.py
@@ -6,7 +6,7 @@ import os
 
 
 def comprobar_ultima_linea(ultima_linea):
-    patron = re.compile(r'^[0-9a-f]{8}\t[0-9a-f]{2}\t100')
+    patron = re.compile(r'^[0-9a-f]{8}(\t|\s+)[0-9a-f]{2}(\t|\s+)100')
     return bool(patron.match(ultima_linea))
 
 def main(f1,f2):
@@ -22,14 +22,18 @@ def main(f1,f2):
     sha256.update(contenido_fichero_2)
     digest_2 = sha256.hexdigest()
 
-    num_filas_f1 = len(contenido_fichero_2.decode().splitlines())
+    # num_filas_f1 = len(contenido_fichero_2.decode().splitlines())
+    num_filas_f1 = sum(1 for _ in open(f1))
+    num_filas_f2 = sum(1 for _ in open(f2))
 
     # Condiciones
     empiezan_igual = contenido_fichero_2.startswith(contenido_fichero_1)
     empieza_con_cero = digest_2.startswith("0")
-    ultima_linea = comprobar_ultima_linea(contenido_fichero_2.decode().splitlines()[num_filas_f1-1])
-    valido = empieza_con_cero and empiezan_igual and ultima_linea
+    ultima_linea = comprobar_ultima_linea(contenido_fichero_2.decode().splitlines()[-1])
+    ultima_linea_seguida = num_filas_f2 == (num_filas_f1 + 1) 
 
+    # Resultados
+    valido = empieza_con_cero and empiezan_igual and ultima_linea and ultima_linea_seguida
     num_0 = len(digest_2)-len(digest_2.lstrip('0'))
     fecha_modif = os.stat(f2).st_mtime
 
